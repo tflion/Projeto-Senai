0d76bf4866fe2863bea17a860180181bded9b8c1
vitor2233 <vitorguilherme2233@gmail.com>
2019-04-29 16:16:06 -0300

    Design do login

--- ProjetoSenai/ProjetoSenai/Form1.Designer.cs
+++ ProjetoSenai/ProjetoSenai/Form1.Designer.cs
@@ -29,20 +29,89 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.lblEsqueceuSenha = new System.Windows.Forms.Label();
+            this.lblEscola = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(90, 120);
+            this.textBox1.Location = new System.Drawing.Point(34, 108);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
             this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(30, 85);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(64, 20);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "E-MAIL:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(30, 149);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(60, 20);
+            this.lblSenha.TabIndex = 3;
+            this.lblSenha.Text = "SENHA:";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(34, 198);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "ENTRAR";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // lblEsqueceuSenha
+            // 
+            this.lblEsqueceuSenha.AutoSize = true;
+            this.lblEsqueceuSenha.Location = new System.Drawing.Point(31, 224);
+            this.lblEsqueceuSenha.Name = "lblEsqueceuSenha";
+            this.lblEsqueceuSenha.Size = new System.Drawing.Size(105, 13);
+            this.lblEsqueceuSenha.TabIndex = 5;
+            this.lblEsqueceuSenha.Text = "Esqueceu a senha ?";
+            // 
+            // lblEscola
+            // 
+            this.lblEscola.AutoSize = true;
+            this.lblEscola.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscola.Location = new System.Drawing.Point(57, 28);
+            this.lblEscola.Name = "lblEscola";
+            this.lblEscola.Size = new System.Drawing.Size(156, 24);
+            this.lblEscola.TabIndex = 6;
+            this.lblEscola.Text = "SENAI CENTRO";
+            this.lblEscola.Click += new System.EventHandler(this.lblEscola_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblEscola);
+            this.Controls.Add(this.lblEsqueceuSenha);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,6 +123,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label lblEsqueceuSenha;
+        private System.Windows.Forms.Label lblEscola;
     }
 }
 
--- ProjetoSenai/ProjetoSenai/Form1.cs
+++ ProjetoSenai/ProjetoSenai/Form1.cs
@@ -16,5 +16,10 @@ namespace ProjetoSenai
         {
             InitializeComponent();
         }
+
+        private void lblEscola_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
