52bcb70dd2f32f3a9763f6ca23e377e68ee75d68
vitor2233 <vitorguilherme2233@gmail.com>
2019-04-29 19:52:20 -0300

    Design do login

---
+++ ProjetoSenai/ProjetoSenai/frmLogin.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ProjetoSenai
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtSenha = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.lblLogin = new MaterialSkin.Controls.MaterialLabel();
+            this.lblEsqueceuSenha = new MaterialSkin.Controls.MaterialLabel();
+            this.lblRegistro = new MaterialSkin.Controls.MaterialLabel();
+            this.btnLogin = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Depth = 0;
+            this.txtEmail.Hint = "Email";
+            this.txtEmail.Location = new System.Drawing.Point(77, 151);
+            this.txtEmail.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.PasswordChar = '\0';
+            this.txtEmail.SelectedText = "";
+            this.txtEmail.SelectionLength = 0;
+            this.txtEmail.SelectionStart = 0;
+            this.txtEmail.Size = new System.Drawing.Size(316, 23);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.UseSystemPasswordChar = false;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Depth = 0;
+            this.txtSenha.Hint = "Senha";
+            this.txtSenha.Location = new System.Drawing.Point(77, 230);
+            this.txtSenha.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '\0';
+            this.txtSenha.SelectedText = "";
+            this.txtSenha.SelectionLength = 0;
+            this.txtSenha.SelectionStart = 0;
+            this.txtSenha.Size = new System.Drawing.Size(316, 23);
+            this.txtSenha.TabIndex = 4;
+            this.txtSenha.UseSystemPasswordChar = false;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(32, 356);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(400, 2);
+            this.materialDivider1.TabIndex = 5;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Depth = 0;
+            this.lblLogin.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblLogin.Location = new System.Drawing.Point(138, 89);
+            this.lblLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(191, 19);
+            this.lblLogin.TabIndex = 6;
+            this.lblLogin.Text = "Faça o login para continuar";
+            // 
+            // lblEsqueceuSenha
+            // 
+            this.lblEsqueceuSenha.AutoSize = true;
+            this.lblEsqueceuSenha.Depth = 0;
+            this.lblEsqueceuSenha.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblEsqueceuSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblEsqueceuSenha.Location = new System.Drawing.Point(47, 389);
+            this.lblEsqueceuSenha.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblEsqueceuSenha.Name = "lblEsqueceuSenha";
+            this.lblEsqueceuSenha.Size = new System.Drawing.Size(142, 19);
+            this.lblEsqueceuSenha.TabIndex = 7;
+            this.lblEsqueceuSenha.Text = "Esqueceu a senha ?";
+            // 
+            // lblRegistro
+            // 
+            this.lblRegistro.AutoSize = true;
+            this.lblRegistro.Depth = 0;
+            this.lblRegistro.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblRegistro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblRegistro.Location = new System.Drawing.Point(300, 389);
+            this.lblRegistro.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblRegistro.Name = "lblRegistro";
+            this.lblRegistro.Size = new System.Drawing.Size(69, 19);
+            this.lblRegistro.TabIndex = 8;
+            this.lblRegistro.Text = "Registrar";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Depth = 0;
+            this.btnLogin.Location = new System.Drawing.Point(221, 295);
+            this.btnLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Primary = true;
+            this.btnLogin.Size = new System.Drawing.Size(172, 36);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "ENTRAR";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 457);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblRegistro);
+            this.Controls.Add(this.lblEsqueceuSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SENAI - serviço de aprendizagem industrial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtEmail;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtSenha;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialLabel lblLogin;
+        private MaterialSkin.Controls.MaterialLabel lblEsqueceuSenha;
+        private MaterialSkin.Controls.MaterialLabel lblRegistro;
+        private MaterialSkin.Controls.MaterialRaisedButton btnLogin;
+    }
+}
+
---
+++ ProjetoSenai/ProjetoSenai/frmLogin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Material Design
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ProjetoSenai
+{
+    public partial class frmLogin : MaterialForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkin = MaterialSkinManager.Instance;
+            materialSkin.AddFormToManage(this);
+            materialSkin.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            //Cores do form
+            materialSkin.ColorScheme = new ColorScheme(
+                Primary.Red500,Primary.Red500,
+                Primary.Red400,Accent.Red400,
+                TextShade.WHITE
+            );
+        }
+
+        private void lblEscola_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
