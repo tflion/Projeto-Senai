8d59586c9962c7f0abc55349ba2d4d05afd2236f
tflion <tfleao07@gmail.com>
2019-05-02 15:09:56 -0300

    Banco de dados Full

---
+++ ProjetoSenai/frmLogin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Material Design
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ProjetoSenai
+{
+    public partial class frmLogin : MaterialForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkin = MaterialSkinManager.Instance;
+            materialSkin.AddFormToManage(this);
+            materialSkin.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            //Cores do form
+            materialSkin.ColorScheme = new ColorScheme(
+                Primary.Red500,Primary.Red500,
+                Primary.Red400,Accent.Red400,
+                TextShade.WHITE
+            );
+        }
+
+        private void lblEscola_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEsqueceuSenha_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            frmFuncionario form = new frmFuncionario();
+            form.Show();
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+            txtSenha.UseSystemPasswordChar = true;
+        }
+    }
+}
---
+++ ProjetoSenai/userPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Material Design
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ProjetoSenai
+{
+    public partial class userPrincipal : UserControl
+    {
+        public userPrincipal()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkin = MaterialSkinManager.Instance;
+            materialSkin.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            //Cores do form
+            materialSkin.ColorScheme = new ColorScheme(
+                Primary.Red500, Primary.Red500,
+                Primary.Red500, Accent.Red400,
+                TextShade.WHITE
+            );
+        }
+    }
+}
